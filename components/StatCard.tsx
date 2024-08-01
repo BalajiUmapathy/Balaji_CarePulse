06f4715588de038090ac501b4887cce7d0f85649
Balaji Umapathy <jaswanthbalamala992004@gmail.com>
2024-08-01 15:20:52 +0530

    Initial commit

---
+++ components/StatCard.tsx
@@ -0,0 +1,41 @@
+import { icons } from 'lucide-react';
+import React from 'react'
+import { number, string } from 'zod';
+import Image from 'next/image';
+import clsx from 'clsx';
+
+
+interface statCardProps {
+    type : 'appointments' | 'pending' | 'cancelled'
+    count: number
+    label: string
+    icon: string
+    
+}
+
+const StatCard = ({count = 0 ,label , icon,type }: statCardProps) => {
+  return (
+    <div
+    className={clsx("stat-card", {
+      "bg-appointments": type === "appointments",
+      "bg-pending": type === "pending",
+      "bg-cancelled": type === "cancelled",
+    })}
+  >
+    <div className="flex items-center gap-4">
+      <Image
+        src={icon}
+        height={32}
+        width={32}
+        alt="appointments"
+        className="size-8 w-fit"
+      />
+      <h2 className="text-32-bold text-white">{count}</h2>
+    </div>
+
+    <p className="text-14-regular">{label}</p>
+  </div>
+  )
+}
+
+export default StatCard
